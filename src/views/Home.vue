94c4c523ceaed619c6c34ae151e790f6cfaf044f
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2025-07-23 08:54:29 +0700

    chore: init project with vue3 and tailwind

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div id="home-page" class="w-full flex justify-center pt-[60px]">
+    <h1 class="text-4xl font-bold text center">Tears Dalu Home Page</h1>
+  </div>
+</template>
