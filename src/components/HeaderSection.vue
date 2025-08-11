769327701f30b7b709a83840ca3282d63d3f813f
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2025-08-11 21:59:58 +0700

    feat: create responsive hero section

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,8 +1,12 @@
 <script setup>
+import NavbarSection from "./NavbarSection.vue";
+import HeroSection from "./HeroSection.vue";
 </script>
 
 <template>
   <header id="header-section"
           class="w-full lg:h-screen bg-alova py-[22px] xl:py-[35px] px-5 md:px-[40px] lg:px-[98px]  xl:px-[109px] grid grid-cols-2 lg:grid-cols-[auto_auto_auto] lg:grid-rows-[100px_auto_100px] gap-4 lg:gap-[21px]">
+    <navbar-section/>
+    <hero-section/>
   </header>
 </template>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="w-full h-[380px] lg:h-auto rounded-2xl overflow-hidden bg-[url(/images/hero-banner.png)] bg-cover bg-center bg-no-repeat col-span-2 lg:col-span-3 "></div>
+  <h1
+    class="text-[clamp(2.7rem,_1.8864115168539326rem_+_2.633426966292135vw,_4.375rem)] text-center text-murko font-hore col-span-2 lg:col-span-1 lg:order-2">
+    Teras Dalu Bandung</h1>
+  <a
+    class="w-full lg:w-auto lg:max-w-[310px] h-[62px] lg:h-auto flex items-center justify-center border-2 border-drakar bg-alova hover:bg-verza rounded-2xl text-3xl xl:text-4xl text-verza hover:text-alova font-hore cursor-pointer lg:order-1"
+    href="#">
+    LIHAT MENU
+  </a>
+  <a
+    class="w-full lg:w-auto lg:max-w-[310px] h-[62px] lg:h-auto flex items-center justify-center border-2 border-drakar bg-alova hover:bg-verza rounded-2xl text-3xl xl:text-4xl text-verza hover:text-alova font-hore cursor-pointer lg:order-3"
+    href="https://kukuhsatrio.site">
+    RESERVASI
+  </a>
+</template>
+
