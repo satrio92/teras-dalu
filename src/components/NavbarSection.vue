52b828c01ac497e42f6bd0a424e814eaf445ea2e
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2025-08-19 22:05:55 +0700

    feat: fix navbar menu mobile onClick

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -18,10 +18,13 @@ const scrollTo = (sectionId) => {
     <ul id="menu"
         :class="[toggleActive ? 'left-0 lg:left-auto' : 'left-[100vw] lg:left-auto']"
         class="lg:flex lg:flex-1 lg:w-auto lg:h-auto lg:bg-transparent lg:p-0 lg:flex-row lg:relative lg:text-alova lg:items-center lg:justify-center lg:gap-8 xl:gap-11 lg:text-2xl font-hore lg:transition-none transition-all duration-700 ease-in flex flex-col items-start  pl-[90px] pt-[110px] fixed w-[100vw] h-[calc(100vh-96px)] bg-alova z-40 top-[96px] lg:top-auto text-murko text-4xl gap-10 lg:ml-[-5rem]">
-      <li class="cursor-pointer hover:text-verza" @click="scrollTo('about-section')">About</li>
-      <li class="cursor-pointer hover:text-verza" @click="scrollTo('menu-section')">Menu</li>
-      <li class="cursor-pointer hover:text-verza" @click="scrollTo('gallery-section')">Gallery</li>
-      <li class="cursor-pointer hover:text-verza" @click="scrollTo('testimonial-section')">Testimoni</li>
+      <li class="cursor-pointer hover:text-verza" @click="scrollTo('about-section'); toggleActive = false">About</li>
+      <li class="cursor-pointer hover:text-verza" @click="scrollTo('menu-section'); toggleActive = false">Menu</li>
+      <li class="cursor-pointer hover:text-verza" @click="scrollTo('gallery-section'); toggleActive = false">Gallery
+      </li>
+      <li class="cursor-pointer hover:text-verza" @click="scrollTo('testimonial-section'); toggleActive = false">
+        Testimoni
+      </li>
       <li
         class="w-auto h-auto  py-3 px-6 hover:bg-verza border-2 border-drakar lg:border-alova rounded-xl text-4xl lg:text-2xl text-murko hover:text-alova lg:text-alova font-hore lg:absolute lg:right-0 cursor-pointer">
         RESERVASI
