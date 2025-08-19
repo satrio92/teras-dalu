fe36bc6b24b3bf4649e668752b0e0387ae0577f7
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2025-08-19 14:41:23 +0700

    feat: create responsive menu section

---
+++ src/components/MenuSection.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MenuCard from "./MenuCard.vue";
+import menusData from "../data/menu.json"
+import {ref, computed} from 'vue'
+
+const showAllMenu = ref(false);
+
+const menus = ref(menusData || [])
+
+const displayedMenu = computed(() => {
+  return showAllMenu.value
+    ? menus.value
+    : menus.value.filter(item => item.featured)
+})
+
+const toggleMenu = () => {
+  showAllMenu.value = !showAllMenu.value;
+}
+</script>
+
+<template>
+  <section id="menu-section"
+           class="w-full h-auto bg-alova px-5 md:px-[40px] lg:px-[98px] xl:px-[109px] py-[26px] lg:py-[98px] flex flex-col gap-16 lg:gap-20 items-center">
+    <h1 class="text-murko text-5xl text-center lg:text-7xl font-hore">Menu Populer Kami</h1>
+    <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-y-6 lg:gap-x-20 lg:gap-9">
+      <MenuCard v-for="item in displayedMenu" :key="item.id" :item="item"/>
+    </div>
+    <button
+      class="border-2 border-drakar text-murko text-2xl font-hore px-7 py-3 lg:py-5 rounded-lg hover:bg-verza hover:text-alova cursor-pointer"
+      @click="toggleMenu">
+      {{ showAllMenu ? 'Lihat Lebih Sedikit' : 'Menu Lainnya' }}
+    </button>
+  </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderSection from "../components/HeaderSection.vue";
 import AboutSection from "../components/AboutSection.vue";
+import MenuSection from "../components/MenuSection.vue";
 </script>
 
 <template>
   <div id="home-page">
     <HeaderSection/>
     <AboutSection/>
+    <MenuSection/>
   </div>
 </template>
